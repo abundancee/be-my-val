c50a83b5dcaea158b2e56b6b41edbd049918b9af
abundance <abbydeew@gmail.com>
2026-02-14 15:08:52 +0100

    Update love message in Page component

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ export default function Page() {
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
           <div className="text-4xl md:text-6xl font-bold my-4" style={{ fontFamily: 'Federant, cursive' }}>
-            Yaaay! I love you!!!
+            Yaayyyyy!!! YOU LOVE ME.
           </div>
         </>
       ) : (
